a739132081e2a99fe916bc3f0488526141615b03
danilojpfreitas <danilojpfreitas@gmail.com>
2022-06-02 12:49:56 -0300

    File: Versão 01

---
+++ script.js
@@ -0,0 +1,9 @@
+// Animação Main
+
+function displayAbout() {
+    var arrow = document.getElementById('animation')
+    arrow.classList.toggle('rotate')
+    var display = document.getElementById('about')
+    display.classList.toggle('active')
+}
+
